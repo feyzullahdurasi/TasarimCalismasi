543c9549bbe23b604bbe763737da41567f92bbf9
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-06-18 19:56:54 +0300

    SwiftUI ile ilk denemeler ve temel işlemler yapıldı. uygulama icon u değiştirme ismi dil desteği stack yapıları text özellikleri farklı ekran boyutlarına boyutlandırma ve ekran çözünürlüklerine göre fotoğraf yükleme gibi detay konuların temelleri atıldı. Görsellik amacı olmadan konuyu anlamak için çeşitli şeyler denendi

--- TasarimCalismasi/ContentView.swift
+++ TasarimCalismasi/ContentView.swift
@@ -9,16 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            let ekranGenislik = geometry.size.width
+            let ekranYukselik = geometry.size.height
+            
+            HStack{
+                Text("Mer")
+                ZStack(alignment: .bottom){
+                    
+                    Rectangle().fill(.gray).frame(width: 250, height: 500)
+                    Image("resim")
+                    
+                    VStack(spacing: 60) {
+                        Text("Lamba").frame(maxWidth: .infinity).background(.yellow)
+                        Spacer().frame(width: 10, height: 1)
+                        KirmiziKare()
+                        YesilKare().frame(width: ekranGenislik/22, height: ekranYukselik/4)
+                        Rectangle().fill(.blue).frame(width: 100, height: 10).padding(.bottom,50)
+                        
+                    }
+                    Yazi(icerik: "Deneme", yaziBoyutu: 20)
+                    
+                }
+                Text("haba")
+                
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct KirmiziKare: View {
+    var body: some View {
+        Rectangle().fill(.red).frame(width: 120, height: 100)
+    }
+}
+
+struct YesilKare: View {
+    var body: some View {
+        Rectangle().fill(.green)
+    }
+}
+
+struct Yazi: View {
+    var icerik = ""
+    var yaziBoyutu = 0
+    var body: some View {
+        Text(icerik).font(.system(size: CGFloat(yaziBoyutu))).bold()
+    }
 }
