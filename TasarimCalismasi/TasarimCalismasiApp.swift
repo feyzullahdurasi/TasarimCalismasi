68e0d4e3ef65830682d3e83e1f7052036c05faef
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-06-18 12:52:10 +0300

    Initial Commit

---
+++ TasarimCalismasi/TasarimCalismasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasarimCalismasiApp.swift
+//  TasarimCalismasi
+//
+//  Created by Feyzullah Durası on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TasarimCalismasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
